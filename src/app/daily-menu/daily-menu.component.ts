1e098821187a0082b49430c66e9e54387f52b8eb
Slater Smith <smithslater@gmail.com>
2019-03-29 15:50:47 -0700

    add top and bottom border to daily menu component

--- src/app/daily-menu/daily-menu.component.ts
+++ src/app/daily-menu/daily-menu.component.ts
@@ -9,8 +9,8 @@ import { DailyMenuItem } from '../models/dailymenu.model';
 export class DailyMenuComponent {
 
   dailyMenu = [
+    new DailyMenuItem("Your Friday Evening Briefing", "Here's what you need to know at the end of the day.", "../../assets/images/evening.jpg", "https://www.nytimes.com/2019/03/29/briefing/mueller-report-brexit-ncaa.html"),
     new DailyMenuItem("Listen to 'Still Processing': Us", "We dissect Jordan Peele's new psychological thriller", "../../assets/images/still-processing.jpg", "https://www.nytimes.com/2019/03/28/podcasts/still-processing-us-jordan-peele.html"),
-    new DailyMenuItem("Your Friday Evening Briefing", "Here's what you need to know at the end of the day.", "../../assets/images/evening.jpg", "https://www.nytimes.com/2019/03/29/briefing/mueller-report-brexit-ncaa.html")
     new DailyMenuItem("Listen to 'The Argument'", "Opinion columnists debate: Who botched the Mueller report?", "../../assets/images/argument.png", "https://www.nytimes.com/2019/03/29/briefing/mueller-report-brexit-ncaa.html")
   ]
 
