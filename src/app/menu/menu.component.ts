c33c72de3831ae601a64e2242795a15f06a3e1ac
Slater Smith <smithslater@gmail.com>
2019-03-29 09:47:19 -0700

    set up variables in menu app component.ts

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -5,11 +5,31 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
+// export class MenuComponent implements OnInit {
+export class MenuComponent {
+  world = "<li>World</li>"
+  us = "<li>U.S.</li>"
+  politics = "<li>Politics</li>"
+  ny = "<li>N.Y.</li>"
+  business = "<li>Business</li>"
+  opinion = "<li>Opinion</li>"
+  tech = "<li>Tech</li>"
+  science = "<li>Science</li>"
+  health = "<li>Health</li>"
+  sports = "<li>Sports</li>"
+  arts = "<li>Arts</li>"
+  books = "<li>Books</li>"
+  style = "<li>Style</li>"
+  food = "<li>Food</li>"
+  travel = "<li>Travel</li>"
+  magazine = "<li>Magazine</li>"
+  tmagazine = "<li>T magazine</li>"
+  realestate = "<li>Real Estate</li>"
+  video = "<li>Video</li>"
 
-  constructor() { }
+  // constructor() { }
 
-  ngOnInit() {
-  }
+  // ngOnInit() {
+  // }
 
 }
