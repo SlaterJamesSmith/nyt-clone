a635f5296aac1a48b40270798955312d7996b2d4
Slater Smith <smithslater@gmail.com>
2019-03-29 14:01:00 -0700

    update daily menu component with single object item

--- src/app/daily-menu/daily-menu.component.ts
+++ src/app/daily-menu/daily-menu.component.ts
@@ -8,6 +8,10 @@ import { DailyMenuItem } from '../models/dailymenu.model';
 })
 export class DailyMenuComponent {
 
+  dailyMenu = [
+    new DailyMenuItem("Listen to 'Still Processing': Us", "We dissect Jordan Peele's new psychological thriller", "../../assets/images/still-processing.jpg", "https://www.nytimes.com/2019/03/28/podcasts/still-processing-us-jordan-peele.html")
+  ]
+
   // constructor() { }
   //
   // ngOnInit() {
