4324c66a496ebf261dc1b8c3667f6495d967c85a
Slater Smith <smithslater@gmail.com>
2019-04-06 20:10:09 -0700

    sucessfully get mock-data to deliver through service, but will not work for some reason when router is part of the constructor in daily menu component

--- src/app/daily-menu/daily-menu.component.ts
+++ src/app/daily-menu/daily-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DailyMenuItem } from '../models/dailymenu.model';
+import { Router } from '@angular/router';
 import { DailymenuService } from '../dailymenu.service';
 
 @Component({
@@ -8,17 +9,21 @@ import { DailymenuService } from '../dailymenu.service';
   styleUrls: ['./daily-menu.component.css'],
   providers: [DailymenuService]
 })
+
 export class DailyMenuComponent implements OnInit {
   dailyMenu: DailyMenuItem[];
-  constructor(private router: Router, private dailymenuService: DailymenuService) {}
+
+  constructor(public router: Router, private dailymenuService: DailymenuService) {}
 
   ngOnInit(){
-
-  }
-
-  goToDetailPage(clickedAlbum: DailyMenuItem) {
-    this.router.navigate(['dailyMenu', clickedAlbum.headline]);
+    this.dailyMenu = this.dailymenuService.getDailyMenu();
   };
+
+  printMenu() {
+    // this.router.navigate(['dailyMenu', clickedAlbum.headline]);
+  };
+
+
   // dailyMenu = [
   //   new DailyMenuItem("Your Friday Evening Briefing", "Here's what you need to know at the end of the day.", "../../assets/images/evening.jpg", "https://www.nytimes.com/2019/03/29/briefing/mueller-report-brexit-ncaa.html"),
   //   new DailyMenuItem("Listen to 'Still Processing': Us", "We dissect Jordan Peele's new psychological thriller", "../../assets/images/still-processing.jpg", "https://www.nytimes.com/2019/03/28/podcasts/still-processing-us-jordan-peele.html"),
--- src/app/dailymenu.service.ts
+++ src/app/dailymenu.service.ts
@@ -7,4 +7,8 @@ export class DailymenuService {
 
   constructor() { }
 
+  getDailyMenu() {
+    return DAILYMENU;
+  }
+
 }
