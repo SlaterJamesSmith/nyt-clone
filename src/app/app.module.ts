3ed310a1181c8b4ff11ced18a62189b187bba1d4
Slater Smith <smithslater@gmail.com>
2019-04-07 12:10:25 -0700

    reorganize impor code at top of page for readability

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+import { routing } from './app.routing';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DailyMenuComponent } from './daily-menu/daily-menu.component';
-import { routing } from './app.routing';
-
 import { masterFirebaseConfig } from './api-keys';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { FormsModule }    from '@angular/forms';
-import { HttpModule }    from '@angular/http';
+
+
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
